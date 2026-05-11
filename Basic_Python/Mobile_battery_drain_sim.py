c7d2ede76899d096dfffabbf3bd6b3b3ba486241
lovevyas <love1252.be21@chitkarauniversity.edu.in>
2026-05-11 13:06:45 +0530

    Remove directory from this branch

--- Basic_Python/Mobile_battery_drain_sim.py
+++
@@ -1,4 +0,0 @@
-import math
-Battery = 100
-drain = int(input("Drain rate: "))
-print(math.ceil(Battery/drain))
--- Basic_Python/number_com_count.py
+++
@@ -1,6 +0,0 @@
-num = int(input())
-count = 0
-while(num%2==0):
-    count+=1
-    num/=2
-print(count)
